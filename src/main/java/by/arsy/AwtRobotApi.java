69535ddcc73f83366013929b70db231031582cc6
Arseni <esenin.magmel@gmail.com>
2025-07-17 15:30:48 +0300

    create project
    start "Hello world"

---
+++ src/main/java/by/arsy/AwtRobotApi.java
@@ -0,0 +1,13 @@
+package by.arsy;
+
+/**
+ * Hello world!
+ *
+ */
+public class AwtRobotApi {
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+
+}
